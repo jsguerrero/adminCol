8a7470fc55b0a1f41f2aab486d33943120cb5b32
Sebastian Guerrero <sebas.dobleguerrero@gmail.com>
2018-09-14 14:44:34 -0500

    Se agrega la vista base 'base_general' y la primer vista 'index.html'.

--- adminColonias/urls.py
+++ adminColonias/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('colonias/', include('colonias.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/colonias/', permanent=True)),
 ]
--- colonias/views.py
+++ colonias/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import cat_codigo_postal, cat_pais, cat_estado, cat_municipio, cat_asentamiento
+
+def index(request):
+    """
+    Vista para la pagina de inicio
+    """
+
+    num_codigos_postales = cat_pais.objects.all().count()
+    num_paises = cat_pais.objects.all().count()
+    num_estados = cat_estado.objects.all().count()
+    num_municipios = cat_municipio.objects.all().count()
+    num_asentamientos = cat_asentamiento.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_codigos_postales':num_codigos_postales,
+                 'num_paises':num_paises,
+                 'num_estados':num_estados,
+                 'num_municipios':num_municipios,
+                 'num_asentamientos':num_asentamientos
+        },
+    )
